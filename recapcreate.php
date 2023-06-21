24dfcd0b89a70d6d9c05f32d11dd63ed5f1c673e
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-06-21 21:08:36 +0800

    recap for create.php

---
+++ recapcreate.php
@@ -0,0 +1,30 @@
+<?php
+include "config.php";
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = isset($_POST['username']) ? $_POST['username']: '';
+
+    $sql = "INSERT INTO users (username) values ($username)";
+
+if($connection->query($sql) === TRUE) {
+    $_SESSION['success_message'] = "Created";
+}else{
+    echo "Error: " . $sql . "<br>" . $connection->error;
+}
+exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Create </title>
+    </head>
+<body>
+    <h1> Account Information </h1>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+    </form>
+</body>
+</html>    
