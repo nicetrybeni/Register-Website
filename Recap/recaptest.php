91eed6b86576dda27685a8fba719e04858c718f9
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-06-25 19:49:29 +0800

    Update recaptest.php

--- Recap/recaptest.php
+++ Recap/recaptest.php
@@ -1,62 +1,56 @@
-<?php 
-#CREATE 
+<?php
+#CREATE 06/25/23
 include "config.php";
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //retrieve data 
-    $username = isset($_POST['username']) ? $_POST['username'] : '';
 
-// insert to DB
+if($_SERVER"REQUEST_METHOD" == "POST") {
+    
+    $username = isset($_POST['username']) ? $_POST['username']: '';
+
 $sql = "INSERT INTO users (username) values ($username)";
 
-    if($connection->($sql) === TRUE) {
-        $_SESSION['success_message'] = "User created succesfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $connection->error;
+    if($connection->query($sql) === TRUE) {
+        $_SESSION['success_message'] = "Created";
+    }else{
+        echo "Error :" . $sql . "<br>" . $connection->error;
     }
-// Show username from DB (READ)
-    echo "<h1> Username: " . $username . "</h1>";
-
-header("Location: index-admin.php");
-exit();
+    exti();
 }
 ?>
 <?php
 #UPDATE
-include "config.php";
+include ("config.php");
 session_start();
-//validation starts here 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = isset($_POST['id']) ? $_POST['id'] : '';
-    $username = isset($_POST['username']) ? $_POST['username'] : '';
-// Update query
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = isset($_POST['id']) ? $_POST['id']: '';
+    $username = isset($_POST['username']) ? $_POST['username']: '';
+
 $sql = "UPDATE users SET username='$username' WHERE id=$id";
 
-    if($connection->query($sql) === TRUE) {
-        $_SESSION['success_message'] = "User updated";
-    }else {
-        echo "Error :" . $sql . "<br>" .$connection->error;
+    if($connection->query($sql) === TRUE){
+        $_SESSION['sucess_message'] = "Updated";
+    }else{
+        echo "Error :" . $sql . "<br>" . $connection->error;
     }
-    mysqli_close($connection)
-
-    header("Location: index-admin.php")
     exit();
-} 
+}
 ?>
 <?php
 #DELETE
 include "config.php";
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //retrieve data
-    $id = isset($_POST['id']) ? $_POST['id'] : '';
-    //Delete query
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = isset($_POST['id']) ? $_POST['id']: '';
+
 $sql = "DELETE FROM users WHERE id=$id";
 
-if ($connection->query($sql) === TRUE) {
-    $_SESSION['success_message'] = "Deleted";
-}else {
-    echo "Error :". $sql . "<br>" . $connection->error;
-}
+    if($connection->query($sql) === TRUE) {
+        $_SESSION['sucess_message'] = "Deleted";
+    }else{
+        echo "Error: ". $sql . "<br>" . $connection->error;
+    }
+    exit();
 }
 ?>
